a9220c5cdad7463b63f29f86cad59519319feda8
Weiyi Guan <u7574167@anu.edu.au>
2025-04-24 19:30:50 +0800

    It mainly implements the full-process management function of payment methods: including list query, data addition/modification, logical deletion, provides complete support for the back-end business interface, and improves development efficiency and data security through MyBatis-Plus encapsulation.

--- web/web-admin/src/main/java/com/nest/renting/web/admin/controller/apartment/PaymentTypeController.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/controller/apartment/PaymentTypeController.java
@@ -3,8 +3,10 @@ package com.nest.renting.web.admin.controller.apartment;
 
 import com.nest.renting.common.result.Result;
 import com.nest.renting.model.entity.PaymentType;
+import com.nest.renting.web.admin.service.PaymentTypeService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,22 +17,28 @@ import java.util.List;
 @RestController
 public class PaymentTypeController {
 
+    @Autowired
+    private PaymentTypeService paymentTypeService;
+
     @Operation(summary = "Retrieve the full list of payment methods")
     @GetMapping("list")
     public Result<List<PaymentType>> listPaymentType() {
-        return Result.ok();
+        List<PaymentType> list = paymentTypeService.list();
+        return Result.ok(list);
     }
 
     @Operation(summary = "Save or update a payment method")
     @PostMapping("saveOrUpdate")
     public Result saveOrUpdatePaymentType(@RequestBody PaymentType paymentType) {
+        paymentTypeService.saveOrUpdate(paymentType);
         return Result.ok();
     }
 
     @Operation(summary = "Delete a payment method by ID")
     @DeleteMapping("deleteById")
     public Result deletePaymentById(@RequestParam Long id) {
-        return Result.ok();
+        paymentTypeService.removeById(id); // MyBatis-Plus will automatically perform logical deletion due to the @TableLogic annotation
+        return Result.ok(); 
     }
 
 }
