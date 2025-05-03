ba9d362293c040fc687641a04947a34a200191ea
u7805726 <u7805726@anu.edu.au>
2025-05-04 07:13:44 +0800

    feat: implement view appointment module with controller, service, and mapper - Add ViewAppointmentController with page - Implement ViewAppointmentService interface and service logic - Add SQL mapping in ViewAppointmentMapper and XML for paginated query - Add support for AppointmentQueryVo and AppointmentVo with nested apartmentInfo

--- web/web-admin/src/main/java/com/nest/renting/web/admin/controller/lease/ViewAppointmentController.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/controller/lease/ViewAppointmentController.java
@@ -1,22 +1,29 @@
 package com.nest.renting.web.admin.controller.lease;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.nest.renting.common.result.Result;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.nest.renting.model.enums.AppointmentStatus;
+import com.nest.renting.web.admin.service.ViewAppointmentService;
 import com.nest.renting.web.admin.vo.appointment.AppointmentQueryVo;
 import com.nest.renting.web.admin.vo.appointment.AppointmentVo;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "Viewing appointment management")
 @RequestMapping("/admin/appointment")
 @RestController
 public class ViewAppointmentController {
+    @Autowired
+    private ViewAppointmentService service;
     @Operation(summary = "Retrieve a paginated list of viewing appointments")
     @GetMapping("page")
     public Result<IPage<AppointmentVo>> page(@RequestParam long current, @RequestParam long size, AppointmentQueryVo queryVo) {
-        return Result.ok();
+        IPage<AppointmentVo> page = new Page<>(current, size);
+        IPage<AppointmentVo> list = service.pageAppointmentByQuery(page, queryVo);
+        return Result.ok(list);
     }
 
     @Operation(summary = "Update viewing appointment status by ID")
