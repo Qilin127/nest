d7773da0ea37601312b0b3638bd909b45319c956
Weiyi Guan <u7574167@anu.edu.au>
2025-05-04 13:40:16 +0800

    Query tag list: Added a new interface method to query the corresponding tag list based on the optional tag type parameter

--- web/web-admin/src/main/java/com/nest/renting/web/admin/controller/apartment/LabelController.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/controller/apartment/LabelController.java
@@ -1,10 +1,15 @@
 package com.nest.renting.web.admin.controller.apartment;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.nest.renting.common.result.Result;
 import com.nest.renting.model.entity.LabelInfo;
 import com.nest.renting.model.enums.ItemType;
+import com.nest.renting.web.admin.service.LabelInfoService;
+
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,11 +18,15 @@ import java.util.List;
 @RestController
 @RequestMapping("/admin/label")
 public class LabelController {
-
+@Autowired
+    private LabelInfoService service;
     @Operation(summary = "Retrieve the tag list by type")
     @GetMapping("list")
     public Result<List<LabelInfo>> labelList(@RequestParam(required = false) ItemType type) {
-        return Result.ok();
+         LambdaQueryWrapper<LabelInfo> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(type != null, LabelInfo::getType, type);
+        List<LabelInfo> list = service.list(queryWrapper);
+        return Result.ok(list);
     }
 
     @Operation(summary = "Add or update tag information")
