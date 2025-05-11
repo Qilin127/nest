31c9601f84292041793e646de110ed01b8023cb1
u7805726 <u7805726@anu.edu.au>
2025-05-11 14:44:14 +1000

    feat: implement LeaseAgreement saveOrUpdate and paginated query across controller, service, and mapper
    - Add saveOrUpdate endpoint in LeaseAgreementController
    - Implement paginated query using AgreementQueryVo and AgreementVo
    - Complete corresponding methods in LeaseAgreementService and LeaseAgreementServiceImpl
    - Connect to custom SQL in LeaseAgreementMapper

--- web/web-admin/src/main/java/com/nest/renting/web/admin/service/impl/LeaseAgreementServiceImpl.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/service/impl/LeaseAgreementServiceImpl.java
@@ -1,10 +1,15 @@
 package com.nest.renting.web.admin.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nest.renting.model.entity.*;
 import com.nest.renting.web.admin.mapper.*;
 import com.nest.renting.web.admin.service.LeaseAgreementService;
 
+import com.nest.renting.web.admin.vo.agreement.AgreementQueryVo;
+import com.nest.renting.web.admin.vo.agreement.AgreementVo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,6 +18,42 @@ import org.springframework.stereotype.Service;
 @Service
 public class LeaseAgreementServiceImpl extends ServiceImpl<LeaseAgreementMapper, LeaseAgreement>
         implements LeaseAgreementService {
+
+    @Autowired
+    private LeaseAgreementMapper leaseAgreementMapper;
+
+    @Override
+    public IPage<AgreementVo> pageAgreementByQuery(IPage<AgreementVo> page, AgreementQueryVo queryVo) {
+
+        return leaseAgreementMapper.pageAgreementByQuery(page, queryVo);
+    }
+
+    @Override
+    public AgreementVo getAgreementById(Long id) {
+
+        // 1. Query lease agreement information
+        LeaseAgreement leaseAgreement = leaseAgreementMapper.selectById(id);
+
+        // 2. Query apartment information
+        ApartmentInfo apartmentInfo = apartmentInfoMapper.selectById(leaseAgreement.getApartmentId());
+
+        // 3. Query room information
+        RoomInfo roomInfo = roomInfoMapper.selectById(leaseAgreement.getRoomId());
+
+        // 4. Query payment type
+        PaymentType paymentType = paymentTypeMapper.selectById(leaseAgreement.getPaymentTypeId());
+
+        // 5. Query lease term
+        LeaseTerm leaseTerm = leaseTermMapper.selectById(leaseAgreement.getLeaseTermId());
+
+        AgreementVo adminAgreementVo = new AgreementVo();
+        BeanUtils.copyProperties(leaseAgreement, adminAgreementVo);
+        adminAgreementVo.setApartmentInfo(apartmentInfo);
+        adminAgreementVo.setRoomInfo(roomInfo);
+        adminAgreementVo.setPaymentType(paymentType);
+        adminAgreementVo.setLeaseTerm(leaseTerm);
+        return adminAgreementVo;
+    }
 
 }
 
