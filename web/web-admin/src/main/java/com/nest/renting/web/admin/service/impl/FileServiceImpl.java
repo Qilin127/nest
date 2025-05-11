808274185be1bc9b9b2877372fd646be5904476b
Qilin Wang <wangqilin_0725@163.com>
2025-05-11 23:34:47 +1000

    Realize the relevant functions of apartment information management - picture upload - Complete the realization of all functions.

--- web/web-admin/src/main/java/com/nest/renting/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/service/impl/FileServiceImpl.java
@@ -15,11 +15,66 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
 
+
 @Service
 public class FileServiceImpl implements FileService {
 
+    @Autowired
+    private MinioClient minioClient;
+
+    @Autowired
+    private MinioProperties minioProperties;
+
     @Override
-    public String upload(MultipartFile file) {
-        return "";
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
+        // Create a bucket
+        boolean bucketExists = minioClient.bucketExists(BucketExistsArgs
+                .builder()
+                .bucket(minioProperties.getBucketName())
+                .build());
+        if (!bucketExists) { // The bucket is created if it does not exist
+            minioClient.makeBucket(MakeBucketArgs
+                    .builder()
+                    .bucket(minioProperties.getBucketName())
+                    .build());
+            // Set permissions (allow yourself to write and others to read)
+            minioClient.setBucketPolicy(SetBucketPolicyArgs
+                    .builder()
+                    .bucket(minioProperties.getBucketName())
+                    .config(createBucketPolicyConfig(minioProperties.getBucketName()))
+                    .build());
+        }
+
+        // Define the unique file name of the file in Minio
+        String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+        // Upload the file to the bucket
+        minioClient.putObject(PutObjectArgs
+                .builder()
+                .bucket(minioProperties.getBucketName())
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .object(filename)
+                .contentType(file.getContentType())
+                .build());
+
+        return String.join("/", minioProperties.getEndpoint(), minioProperties.getBucketName(), filename);
+    }
+
+    /**
+     * Everyone is allowed to read the content of the bucketName bucket.
+     * By default, only oneself is permitted to write
+     */
+    private String createBucketPolicyConfig(String bucketName) {
+
+        return """
+                {
+                  "Statement" : [ {
+                    "Action" : "s3:GetObject",
+                    "Effect" : "Allow",
+                    "Principal" : "*",
+                    "Resource" : "arn:aws:s3:::%s/*"
+                  } ],
+                  "Version" : "2012-10-17"
+                }
+                """.formatted(bucketName);
     }
 }
