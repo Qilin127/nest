9e3494ec293f84689a5b7d2e71090e70b358f19d
u7805726 <u7805726@anu.edu.au>
2025-08-04 15:09:47 +1000

    feat: add API to update user account status by ID with Swagger documentation

--- web/web-admin/src/main/java/com/nest/renting/web/admin/controller/user/UserInfoController.java
+++ web/web-admin/src/main/java/com/nest/renting/web/admin/controller/user/UserInfoController.java
@@ -23,6 +23,16 @@ public class UserInfoController {
     @Autowired
     private UserInfoService userInfoService;
 
+    /**
+     * Paginated Query of User Information.
+     * This method handles GET requests to retrieve a paginated list of user information.
+     * It supports optional filtering by phone number and account status.
+     *
+     * @param current the current page number
+     * @param size the number of records per page
+     * @param queryVo the query object containing optional filter parameters (phone, status)
+     * @return a paginated result wrapped in a standard response object
+     */
     @Operation(summary = "Paginated Query of User Information")
     @GetMapping("page")
     public Result<IPage<UserInfo>> pageUserInfo(@RequestParam long current, @RequestParam long size, UserInfoQueryVo queryVo) {
@@ -34,6 +44,14 @@ public class UserInfoController {
         return Result.ok(result);
     }
 
+    /**
+     * Update User Account Status by ID.
+     * This method handles POST requests to update the status of a user account based on user ID.
+     *
+     * @param id the ID of the user to update
+     * @param status the new account status to set
+     * @return a standard success response
+     */
     @Operation(summary = "Update Account Status by User ID")
     @PostMapping("updateStatusById")
     public Result updateStatusById(@RequestParam Long id, @RequestParam BaseStatus status) {
