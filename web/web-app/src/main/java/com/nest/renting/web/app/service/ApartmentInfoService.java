3b016a6335ddf3c66c4d36b536583df03bc1bd33
Qilin Wang <wangqilin_0725@163.com>
2025-09-21 19:22:59 +1000

    feat(apartment): add getDetailById API to fetch apartment details

--- web/web-app/src/main/java/com/nest/renting/web/app/service/ApartmentInfoService.java
+++ web/web-app/src/main/java/com/nest/renting/web/app/service/ApartmentInfoService.java
@@ -2,10 +2,13 @@ package com.nest.renting.web.app.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.nest.renting.model.entity.ApartmentInfo;
+import com.nest.renting.web.app.vo.apartment.ApartmentDetailVo;
 import com.nest.renting.web.app.vo.apartment.ApartmentItemVo;
 
 public interface ApartmentInfoService extends IService<ApartmentInfo> {
 
     ApartmentItemVo getApartmentItemVoById(Long apartmentId);
+
+    ApartmentDetailVo getDetailById(Long id);
 
 }
